0359e3c50a384d2a875da333eb71d6785c88ac99
kkoehler4070 <koehler.62@gmail.com>
2026-02-16 12:27:34 -0500

    Update multiplayer_spawner.gd
    Added some logging

--- scripts/server/multiplayer_spawner.gd
+++ scripts/server/multiplayer_spawner.gd
@@ -7,9 +7,13 @@ func _ready() -> void:
 	multiplayer.peer_connected.connect(spawn_player)
 
 func spawn_player(id:int) -> void:
-	if not multiplayer.is_server(): return
+	if not multiplayer.is_server(): 
+		print("Trying to spawn, but I am not the server . . .")
+		return
 	
 	var player: Node = network_player.instantiate()
 	player.name = str(id)
 	
 	get_node(spawn_path).call_deferred("add_child", player)
+	
+	print("Player spawned")
