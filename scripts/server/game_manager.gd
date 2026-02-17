b7e2375a111af5cf920ba8efc3b85c1df41ad71c
kkoehler4070 <koehler.62@gmail.com>
2026-02-17 10:01:21 -0500

    Game Manager
    Added game manager as a singleton and added initial timer logic and states to the game manager.

--- scripts/server/game_manager.gd
+++ scripts/server/game_manager.gd
@@ -1,11 +1,33 @@
 extends Node
 
+enum State {WAITING, RUNNING, END}
+var current_game_state:State
+var timer:Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	current_game_state = State.WAITING
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not Globals.is_server:
+		# Only the server gets to do things
+		return
+		
+	if current_game_state == State.WAITING and (multiplayer.get_peers().size() +1) >= 2:
+		current_game_state = State.RUNNING
+	
+		# Create the timer
+		timer = Timer.new()
+		add_child(timer)
+		timer.wait_time = 60.0
+		timer.one_shot = true
+		timer.timeout.connect(_on_timer_timeout)
+		timer.start()
+		print("timer started")
+		
+func _on_timer_timeout():
+	current_game_state = State.END
+	print("Timer finished")
+	timer.queue_free()
