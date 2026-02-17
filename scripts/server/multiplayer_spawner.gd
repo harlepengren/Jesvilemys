26616cc6f588df9f616852231b712bc3f429bb1d
kkoehler4070 <koehler.62@gmail.com>
2026-02-17 06:36:12 -0500

    Remove Player
    Added code to remove player when they leave the game.

--- scripts/server/multiplayer_spawner.gd
+++ scripts/server/multiplayer_spawner.gd
@@ -5,6 +5,7 @@ extends MultiplayerSpawner
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	multiplayer.peer_connected.connect(spawn_player)
+	multiplayer.peer_disconnected.connect(remove_player)
 
 func spawn_player(id:int) -> void:
 	print("peer_connected fired for id: ", id)
@@ -19,3 +20,12 @@ func spawn_player(id:int) -> void:
 	get_node(spawn_path).add_child(player)
 	
 	print("Tree:",get_tree().root.get_children())  # See what's under root
+	
+func remove_player(id: int) -> void:
+	if not multiplayer.is_server():
+		return
+
+	var player = get_node(spawn_path).get_node_or_null(str(id))
+	if player:
+		player.queue_free()
+		print("Removed player: ", id)
